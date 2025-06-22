2487b0066833e5e545799f63892534d61a3deb23
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2025-06-22 18:03:46 +0100

    update

---
+++ ai-ticket-assistant/models/user.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose"
+import { isStringObject } from "util/types"
+
+
+const userSchema = new mongoose.Schema({
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    role : {type: String, default: "user", enum : ["user", "moderator", "admin"]},
+    skills: [String],
+    createdAt: {type:Date, default: Date.now},
+    
+})
+
+
+export default mongoose.model("User", userSchema)
